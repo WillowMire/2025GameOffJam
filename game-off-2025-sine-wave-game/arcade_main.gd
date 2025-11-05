b3ea573995fc323895bed8ed888db54b82b763bf
WillowMire <wsettlemire50@gmail.com>
2025-11-05 03:44:14 -0600

    Added Camera to player object, made it so you can move the sin wave's offsets with arrow keys, adjusted some physics quirks, and made it so sine wave renders around the player.

--- game-off-2025-sine-wave-game/arcade_main.gd
+++ game-off-2025-sine-wave-game/arcade_main.gd
@@ -2,12 +2,16 @@ extends Node2D
 class_name _ArcadeMain
 
 @export var DEBUG : bool
+@export var player : RigidBody2D
 
 # GOAL
-# get collision with line to work
+# make sine wave controllable
+# make sin wave follow player
 
 var sin_collision : CollisionShape2D
 var seg : SegmentShape2D
+
+var sin_x_bounding_range : float
 
 @export var line : Line2D
 @export var vect_array : Array[Vector2]
@@ -15,8 +19,10 @@ var seg : SegmentShape2D
 @export var b_var : float
 @export var c_var : float
 @export var d_var : float
+@export var control_speed : float
 
 func _ready() -> void:
+	set_wave_x_range()
 	if DEBUG:
 		$DEBUG.visible = true
 		$DEBUG/A_Edit.text = str(a_var)
@@ -26,14 +32,18 @@ func _ready() -> void:
 	draw_wave()
 	start_sin_collision()
 
-func _process(delta: float) -> void:
-	pass
-	c_var += delta * 100
+#might change to _physics_process if theres a problem
+func _physics_process(delta: float) -> void:
+	set_wave_x_range()
+	move_wave_data(delta)
 	draw_wave()
-	update_sin_collision()
+	update_sin_collision() #this may cause problems later, if so try making this and the draw function into lerps
+
+func set_wave_x_range():
+	pass #var player.get_transform().x
 
 var sin_collision_array : Array[CollisionShape2D]
-func start_sin_collision():
+func start_sin_collision() -> void:
 	for i in vect_array.size() - 1:
 		var inst = CollisionShape2D.new()
 		var _seg = SegmentShape2D.new()
@@ -43,16 +53,27 @@ func start_sin_collision():
 		$WaveCollision.add_child(inst)
 		sin_collision_array.append(inst)
 
-func update_sin_collision():
+func update_sin_collision() -> void:
 	for i in vect_array.size() - 1:
 		var _seg = SegmentShape2D.new()
 		_seg.a = vect_array[i]
 		_seg.b = vect_array[i+1]
 		sin_collision_array[i].shape = _seg
 
+func move_wave_data(delta : float) -> void:
+	if Input.is_action_pressed("up"):
+		d_var -= delta * control_speed
+	if Input.is_action_pressed("down"):
+		d_var += delta * control_speed
+	if Input.is_action_pressed("left"):
+		c_var += delta * control_speed
+	if Input.is_action_pressed("right"):
+		c_var -= delta * control_speed
+
 func draw_wave() -> void: 
+	var j = player.position.x
 	vect_array.clear()
-	for x in range(0, 1001, 20):
+	for x in range(j - 500, j + 501, 20):
 		var i = Vector2(x, sin_func_math(x, a_var, b_var, c_var, d_var))
 		vect_array.append(i)
 	line.set_points(vect_array)
