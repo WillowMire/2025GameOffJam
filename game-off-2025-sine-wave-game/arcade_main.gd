c92fce73826a0aaeb12f96790b103af5c0ad1d1d
WillowMire <wsettlemire50@gmail.com>
2025-11-07 02:33:19 -0600

    Experimented with different collision types for wave, changed movement controls to feel a little smoother

--- game-off-2025-sine-wave-game/arcade_main.gd
+++ game-off-2025-sine-wave-game/arcade_main.gd
@@ -3,10 +3,10 @@ class_name _ArcadeMain
 
 @export var DEBUG : bool
 @export var player : RigidBody2D
+@export var cam : Camera2D
 
 # GOAL
-# make sine wave controllable
-# make sin wave follow player
+# add sin wave function UI in top left
 
 var sin_collision : CollisionShape2D
 var seg : SegmentShape2D
@@ -15,14 +15,16 @@ var sin_x_bounding_range : float
 
 @export var line : Line2D
 @export var vect_array : Array[Vector2]
+@export_group("Wave Variables")
 @export var a_var : float
 @export var b_var : float
 @export var c_var : float
 @export var d_var : float
-@export var control_speed : float
+@export var min_control_speed : float
+@export var max_control_speed : float
+@export var move_control_lerp_time : float
 
 func _ready() -> void:
-	set_wave_x_range()
 	if DEBUG:
 		$DEBUG.visible = true
 		$DEBUG/A_Edit.text = str(a_var)
@@ -31,18 +33,16 @@ func _ready() -> void:
 		$DEBUG/D_Edit.text = str(d_var)
 	draw_wave()
 	start_sin_collision()
+	#poly_start_sin_collision()
 
 #might change to _physics_process if theres a problem
 func _physics_process(delta: float) -> void:
-	set_wave_x_range()
 	move_wave_data(delta)
 	draw_wave()
 	update_sin_collision() #this may cause problems later, if so try making this and the draw function into lerps
+	#poly_update_sin_collision()
 
-func set_wave_x_range():
-	pass #var player.get_transform().x
-
-var sin_collision_array : Array[CollisionShape2D]
+var sin_collision_array : Array[CollisionShape2D] #change to poly or normal depending on what sincollision im doing
 func start_sin_collision() -> void:
 	for i in vect_array.size() - 1:
 		var inst = CollisionShape2D.new()
@@ -60,20 +60,47 @@ func update_sin_collision() -> void:
 		_seg.b = vect_array[i+1]
 		sin_collision_array[i].shape = _seg
 
+func poly_start_sin_collision() -> void:
+	var inst = CollisionPolygon2D.new()
+	inst.polygon.append(vect_array[0] + Vector2(0, 500))
+	for i in vect_array.size() - 1:
+		inst.polygon.append(vect_array[i])
+	inst.polygon.append(vect_array[vect_array.size() - 1] + Vector2(0, 500))
+	sin_collision_array.append(inst)
+	$WaveCollision.add_child(inst)
+
+func poly_update_sin_collision() -> void:
+	sin_collision_array[0].polygon.clear()
+	sin_collision_array[0].polygon.append(vect_array[0] + Vector2(0, 500))
+	for i in vect_array.size() - 1:
+		sin_collision_array[0].polygon.append(vect_array[i])
+	sin_collision_array[0].polygon.append(vect_array[vect_array.size() - 1] + Vector2(0, 500))
+
+var v_move_timer : float
+var h_move_timer : float
 func move_wave_data(delta : float) -> void:
-	if Input.is_action_pressed("up"):
-		d_var -= delta * control_speed
-	if Input.is_action_pressed("down"):
-		d_var += delta * control_speed
-	if Input.is_action_pressed("left"):
-		c_var += delta * control_speed
-	if Input.is_action_pressed("right"):
-		c_var -= delta * control_speed
+	pass
+	var v_input = Input.get_axis("up", "down")
+	var h_input = Input.get_axis("left", "right")
+	
+	if v_input != 0:
+		v_move_timer += delta / move_control_lerp_time
+		var v_lerp = clampf(lerpf(min_control_speed, max_control_speed, v_move_timer), min_control_speed, max_control_speed)
+		d_var += delta * v_lerp * v_input
+	elif v_input == 0:
+		v_move_timer = 0
+	
+	if h_input != 0:
+		h_move_timer += delta / move_control_lerp_time
+		var h_lerp = clampf(lerpf(min_control_speed, max_control_speed, h_move_timer), min_control_speed, max_control_speed)
+		c_var -= delta * h_lerp * h_input
+	elif h_input == 0:
+		h_move_timer = 0
 
 func draw_wave() -> void: 
 	var j = player.position.x
 	vect_array.clear()
-	for x in range(j - 500, j + 501, 20):
+	for x in range(j - 1200, j + 1201, 20):
 		var i = Vector2(x, sin_func_math(x, a_var, b_var, c_var, d_var))
 		vect_array.append(i)
 	line.set_points(vect_array)
